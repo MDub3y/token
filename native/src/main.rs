67720b1e1b54a27a6148d1370c015634df9a9c5b
Awesxme <manasvi.d007@gmail.com>
2026-07-21 18:37:02 +0000

    create account ix

--- native/src/main.rs
+++ native/src/main.rs
@@ -1,3 +1,13 @@
+use home::home_dir;
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::{
+    commitment_config::CommitmentConfig,
+    signature::{Keypair, read_keypair_file},
+    system_instruction,
+};
+use spl_token_2022::{extension::ExtensionType, state::Mint};
+use spl_token_metadata_interface::state::TokenMetadata;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let connection = RpcClient::new_with_commitment(
         "http://127.0.0.1:8899".to_string(),
@@ -10,10 +20,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let payer = read_keypair_file(&keypair_path)
         .map_err(|e| format!("Failed to read keypair from {:?}: {}", keypair_path, e))?;
 
-    println!("Payer: {}", payer.pubkey());
+    println!("Payer: {}", payer.pubkey);
 
     let mint = Keypair::new();
-    println!("Mint: {}", mint.pubkey());
+    println!("Mint: {}", mint.pubkey);
 
     let name = "only possible on solana".to_string();
     let symbol = "OPOS".to_string();
@@ -34,4 +44,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let total_space = mint_space + metadata_space;
 
     let lamports = connection.get_minimum_balance_for_rent_exemption(total_space)?;
+
+    let create_account_ix = system_instruction::create_account(
+        &payer.pubkey(),
+        &mint.pubkey(),
+        lamports,
+        total_space as u64,
+        &spl_token_2022::id(),
+    );
 }
