aa357ffb81a68f062baf88a06a05304d7af0092f
Awesxme <manasvi.d007@gmail.com>
2026-07-21 18:28:29 +0000

    home dir and payer keypayer

--- native/src/main.rs
+++ native/src/main.rs
@@ -3,4 +3,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "http://127.0.0.1:8899".to_string(),
         CommitmentConfig::confirmed(),
     );
+
+    let home = home_dir().ok_or("Could not locate home directory")?;
+    let keypair_path = home.join("~/.config/solana/id.json");
+
+    let payer = read_keypair_file(&keypair_path)
+        .map_err(|e| format!("Failed to read keypair from {:?}: {}", keypair_path, e))?;
+
+    println!("Payer: {}", payer.pubkey());
 }
